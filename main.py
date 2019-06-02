64b2f165d1f1820e55ae4cbd99fb61fccee6558f
Nikita <dennikita03@gmail.com>
2019-06-02 19:50:12 +0500

    First workish bot

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 # coding=<UTF-8>
 # !usr/bin/env python3
 
+
+from pprint import pprint
+from subject import *
 import telebot
 from telebot import apihelper
+from telebot import types
 
+
+apihelper.proxy = {'https': 'socks5://207.97.174.134:1080'}
 bot = telebot.TeleBot('836764194:AAHd4hDv6wvLNXrLqQcQbnVq0Kv1HVvIl1g')
-apihelper.proxy = {'https': 'socks5://110.49.101.58:1080'}
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     """/start and /help."""
-    bot.reply_to(message, 'Howdy, how are you doing?')
+    bot.send_message(message.chat.id, '''
+    Привет, я Telegram-bot!\nЗдесь ты можешь поделиться готовой домашкой или получить ее!
+    \n/giveHW - Поделится домашкой\n/getHW - Получить домашку''')
+
+
+@bot.message_handler(commands=['getHW'])
+def get_home_work(messgae):
+    """Начало обработки функции getHW"""
+
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(bio, geo, art, his, ikb, lit, math, phys, chm, com)
+    bot.send_message(messgae.chat.id, 'Отлично, ты хочешь получить готовое решение домашки!\nВыбери предмет:\n\n',
+                     reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    bot.send_message(call.message.chat.id, call.data)
+    pprint(call.message.chat.id)
 
 
 bot.polling()
-
-
